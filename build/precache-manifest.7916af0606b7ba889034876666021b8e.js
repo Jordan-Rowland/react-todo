d455782f2dd96840443808744f08e6c13307131f
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2020-02-08 16:35:24 -0800

    updates gitignore for build

---
+++ build/precache-manifest.7916af0606b7ba889034876666021b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a3864d3469cccc1aef4d9ada03b4f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a699140eafd405f6b4a1",
+    "url": "/static/css/main.14e1a238.chunk.css"
+  },
+  {
+    "revision": "bf23f37a406cfaf7ea53",
+    "url": "/static/js/2.912dfd2d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.912dfd2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a699140eafd405f6b4a1",
+    "url": "/static/js/main.fa973780.chunk.js"
+  },
+  {
+    "revision": "d84a3e19d8a947dff9dd",
+    "url": "/static/js/runtime-main.b2044548.js"
+  }
+]);
