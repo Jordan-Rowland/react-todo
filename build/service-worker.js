f8d66cbfbd8de2a90a5ac0eb2e174db0fc6097cc
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2020-02-08 20:55:39 -0800

    correct route for pre-cache

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bcf96799bfa5f6ab612818df846a8d45.js"
+  "./precache-manifest.bcf96799bfa5f6ab612818df846a8d45.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -34,6 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
+
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
